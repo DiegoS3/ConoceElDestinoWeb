b90cb9934e022eb02c75e4baada30d9cbaeffed8
Diego Sanchez <diegozj92@gmail.com>
2022-02-12 20:30:29 +0100

    Created Navbar not styles yet

--- ConoceElDestino/src/app/shared/shared.module.ts
+++ ConoceElDestino/src/app/shared/shared.module.ts
@@ -1,43 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule, DatePipe } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
-import { DropdownModule } from 'primeng/dropdown';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { SidebarModule } from 'primeng/sidebar';
-import { TooltipModule } from 'primeng/tooltip';
-import { TableModule } from 'primeng/table';
-import { ChartModule } from 'primeng/chart';
-import { ButtonModule } from 'primeng/button';
-import { OverlayPanelModule } from 'primeng/overlaypanel';
-import { CheckboxModule } from 'primeng/checkbox';
-import { InputTextModule } from 'primeng/inputtext';
-import { CalendarModule } from 'primeng/calendar';
 import { RouterModule } from '@angular/router';
-import { SelectButtonModule } from 'primeng/selectbutton';
+import {MenubarModule} from 'primeng/menubar';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const COMPONENTS = [
+  NavbarComponent,
+]
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ...COMPONENTS
+  ],
   imports: [
     CommonModule,
     TranslateModule.forChild({
       extend: true,
     }),
     RouterModule,
-    FormsModule,
-    DropdownModule,
-    InputTextModule,
-    ReactiveFormsModule,
-    SidebarModule,
-    TableModule,
-    ButtonModule,
-    OverlayPanelModule,
-    TooltipModule,
-    CheckboxModule,
-    InputTextModule,
-    CalendarModule,
-    ChartModule,
-    SelectButtonModule
+    MenubarModule
   ],
   providers: [DatePipe],
+  exports: [...COMPONENTS]
 })
 export class SharedModule {}
