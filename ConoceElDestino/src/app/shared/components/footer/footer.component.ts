a099bda440e15111b1972bc88665d4088262ff90
Diego Sánchez <diegozj92@gmail.com>
2022-03-06 16:42:49 +0100

    New styles, added sections and items

--- ConoceElDestino/src/app/shared/components/footer/footer.component.ts
+++ ConoceElDestino/src/app/shared/components/footer/footer.component.ts
@@ -8,6 +8,48 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  services = [
+    {
+      label: 'Visitas Guiadas',
+      navigate: ''
+    },
+    {
+      label: 'Georutas',
+      navigate: ''
+    },
+    {
+      label: 'Experiencias',
+      navigate: ''
+    },
+    {
+      label: 'Viajes',
+      navigate: ''
+    },
+  ];
+
+  aboutUs = [
+    {
+      label: 'Sobre Conoce El Destino',
+      navigate: ''
+    },
+    {
+      label: 'Contactanos',
+      navigate: ''
+    },
+    {
+      label: 'Politica Privacidad',
+      navigate: ''
+    },
+    {
+      label: 'Terminos y Condiciones',
+      navigate: ''
+    },
+    {
+      label: 'Trabaja Con Nosotros',
+      navigate: ''
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
