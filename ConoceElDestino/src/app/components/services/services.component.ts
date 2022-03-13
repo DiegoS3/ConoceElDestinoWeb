5208c4e0cafff819a1bdbee79a5b3f24a0e062af
Diego Sánchez <diegozj92@gmail.com>
2022-03-13 19:26:19 +0100

    Card created

--- ConoceElDestino/src/app/components/services/services.component.ts
+++ ConoceElDestino/src/app/components/services/services.component.ts
@@ -7,31 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesComponent implements OnInit {
   products: any[] = [];
-  responsiveOptions;
 
   constructor() {
-    this.products.push({name: 'Visitas Guiadas', image: '29366.jpg'})
-    this.products.push({name: 'Georutas', image: '29367.jpg'})
-    this.products.push({name: 'Experiencias', image: '29368.jpg'})
-    this.products.push({name: 'Viajes', image: '29369.jpg'})
-    this.responsiveOptions = [
-      {
-          breakpoint: '1024px',
-          numVisible: 4,
-          numScroll: 4
-      },
-      {
-          breakpoint: '768px',
-          numVisible: 2,
-          numScroll: 2
-      },
-      {
-          breakpoint: '560px',
-          numVisible: 1,
-          numScroll: 1
-      }
-  ];
-   }
+    this.products.push({ name: 'Visitas Guiadas', image: '29366.jpg', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', price: 20 })
+    this.products.push({ name: 'Georutas', image: '29367.jpg', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', price: 20 })
+    this.products.push({ name: 'Experiencias', image: '29368.jpg', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', price: 20 })
+    this.products.push({ name: 'Viajes', image: '29369.jpg', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', price: 20 })
+    this.products.push({ name: 'Viajes', image: '29369.jpg', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', price: 20 })
+  }
 
   ngOnInit(): void {
   }
--- ConoceElDestino/src/app/shared/components/generic-carousel-products/generic-carousel-products.component.ts
+++ ConoceElDestino/src/app/shared/components/generic-carousel-products/generic-carousel-products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
 
 @Component({
   selector: 'app-generic-carousel-products',
@@ -8,9 +8,41 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 })
 export class GenericCarouselProductsComponent implements OnInit {
 
-  constructor() { }
+  @Input() carouselData: any;
 
+  responsiveOptions;
+
+  constructor() {
+    this.responsiveOptions = [
+      {
+        breakpoint: '1920px',
+        numVisible: 5,
+        numScroll: 1
+      },
+      {
+        breakpoint: '1520px',
+        numVisible: 4,
+        numScroll: 1
+      },
+      {
+        breakpoint: '1400px',
+        numVisible: 3,
+        numScroll: 1
+      },
+      {
+        breakpoint: '1100px',
+        numVisible: 2,
+        numScroll: 1
+      },
+      {
+        breakpoint: '560px',
+        numVisible: 1,
+        numScroll: 1
+      }
+    ];
+  }
   ngOnInit(): void {
+
   }
 
 }
