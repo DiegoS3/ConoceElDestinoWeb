0e3a8c3fbc2d52c188127f9714fd966c7c2ceabe
Diego Sánchez <diegozj92@gmail.com>
2022-03-13 20:32:02 +0100

    item workinf

--- ConoceElDestino/src/app/components/services/services.component.ts
+++ ConoceElDestino/src/app/components/services/services.component.ts
@@ -9,7 +9,7 @@ export class ServicesComponent implements OnInit {
   products: any[] = [];
 
   constructor() {
-    this.products.push({ name: 'Visitas Guiadas', image: '29366.jpg', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', price: 20 })
+    this.products.push({ name: 'Visitas Guiadas', image: '29366.jpg', description: 'Lorem ipsum dolor sit amet, consecteur adipiscing elit.', price: 20 })
     this.products.push({ name: 'Georutas', image: '29367.jpg', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', price: 20 })
     this.products.push({ name: 'Experiencias', image: '29368.jpg', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', price: 20 })
     this.products.push({ name: 'Viajes', image: '29369.jpg', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', price: 20 })
