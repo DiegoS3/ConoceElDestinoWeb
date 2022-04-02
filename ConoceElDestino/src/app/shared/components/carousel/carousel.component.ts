862106c56a6a76415c6d1fbed45629a000b84441
Diego Sánchez <diegozj92@gmail.com>
2022-04-02 13:56:35 +0200

    Rework main-carousel

--- ConoceElDestino/src/app/shared/components/carousel/carousel.component.ts
+++ ConoceElDestino/src/app/shared/components/carousel/carousel.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ServicesService } from 'src/app/services/services.service';
+import { GenericCarouselItemData } from '../../models/generic-carousel.model';
 
 @Component({
   selector: 'app-carousel',
@@ -7,15 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarouselComponent implements OnInit {
 
-  products: any[] = [
-    { name: 'a', image: '29366.jpg' },
-    { name: 'b', image: '29367.jpg' },
-    { name: 'c', image: '29368.jpg' },
-    { name: 'd', image: '29369.jpg' },
+  products: GenericCarouselItemData[] = [
+    { id: '1', name: 'Visitas Guiadas', image: '29366.jpg', description: 'Lorem ipsum dolor sit amet, consecteur adipiscing elit.', price: 20, rating: 4 },
+    { id: '2', name: 'Expncias', image: '29368.jpg', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', price: 20, rating: 3.7 },
+    { id: '3', name: 'Georutas', image: '29367.jpg', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', price: 20, rating: 5 },
+    { id: '4', name: 'Experiencias', image: '29368.jpg', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', price: 20, rating: 2.8 },
   ];
   responsiveOptions;
 
-  constructor() {
+  constructor(
+    private servicesService: ServicesService,
+    private router: Router) {
     this.responsiveOptions = [
       {
         breakpoint: '1024px',
@@ -36,6 +41,11 @@ export class CarouselComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  onClick(content: GenericCarouselItemData) {
+    this.servicesService.setSelectedService(content);
+    this.router.navigate([`services/${content.name}/${content.id}`]);
   }
 
 }
