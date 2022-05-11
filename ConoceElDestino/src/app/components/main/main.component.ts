ecd4d1db4ea5b0c1e22f3cb1a8e1fa24c4223154
Diego Sánchez <diegozj92@gmail.com>
2022-05-11 13:58:19 +0200

    generic service

--- ConoceElDestino/src/app/components/main/main.component.ts
+++ ConoceElDestino/src/app/components/main/main.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { GenericService } from 'src/app/services/generic.service';
+import { endpoint } from 'src/environments/apis/apis';
 
 @Component({
   selector: 'app-main',
@@ -9,30 +12,37 @@ export class MainComponent implements OnInit {
 
   products: any[] = [];
 
-  constructor() {
-    this.products.push({
-      name: 'Visitas Guiadas', image: '29366.jpg', shortDescription: 'Visitas culturales, de naturaleza, tematizadas, industriales, etnográficas, etc, con una interpretación dinámica y divertida.',
-      description: 'Ven a descubrir alguno de los atractivos de la provincia en una de ' +
-        'nuestras visitas guiadas. Visitas culturales, de naturaleza, tematizadas, industriales, etnográficas, etc, con una interpretación dinámica y divertida'
-    })
-    this.products.push({
-      name: 'Georutas', image: '29367.jpg', shortDescription: 'Rutas de senderismo centradas en los valores geológicos de nuestro territorio.',
-      description: 'Rutas de senderismo centradas en los valores geológicos de nuestro territorio.' +
-        ' La mineralización de Mercurio de Almadén, la Pompeya Carbonífera de Puertollano o el Campo Volcánico de Calatrava como hechos que nos conducen por el camino' +
-        ' de la historia geológica de C.Real.'
-    })
-    this.products.push({
-      name: 'Experiencias', image: '29368.jpg', shortDescription: 'Ciudad Real tiene muchos lugares por descubrir… ¡atreveteydejáte encandilar por su mágia tan  desconocida como sorprendente!',
-      description: 'Ciudad Real tiene muchos lugares por descubrir… ¡atreveteydejáte encandilar por su mágia tan  desconocida como sorprendente! Experiencias gastronómicas, paquetes de fin de semana, turismo de salud...'
-    })
-    this.products.push({
-      name: 'Viajes', image: '29369.jpg', shortDescription: 'Viajes y escapadas en grupo',
-      description: 'Viajes y escapadas en grupo. Si eres de la provincia y quieres conocer sitios nuevos de la Península Ibérica, pero no tienes tiempo para organizarlo, ¡no te preocupes! Realizamos circuitos para aventureros con diferentes inquietudes y con todo organizado. ;)'
-    })
+  private categoryListSubject = new BehaviorSubject<any[]>([]);
+  categoryList$: Observable<any>
+
+  constructor(private genericService: GenericService) {
+
+    this.categoryList$ = this.categoryListSubject.asObservable();
+    // this.products.push({
+    //   name: 'Visitas Guiadas', image: '29366.jpg', shortDescription: 'Visitas culturales, de naturaleza, tematizadas, industriales, etnográficas, etc, con una interpretación dinámica y divertida.',
+    //   description: 'Ven a descubrir alguno de los atractivos de la provincia en una de ' +
+    //     'nuestras visitas guiadas. Visitas culturales, de naturaleza, tematizadas, industriales, etnográficas, etc, con una interpretación dinámica y divertida'
+    // })
+    // this.products.push({
+    //   name: 'Georutas', image: '29367.jpg', shortDescription: 'Rutas de senderismo centradas en los valores geológicos de nuestro territorio.',
+    //   description: 'Rutas de senderismo centradas en los valores geológicos de nuestro territorio.' +
+    //     ' La mineralización de Mercurio de Almadén, la Pompeya Carbonífera de Puertollano o el Campo Volcánico de Calatrava como hechos que nos conducen por el camino' +
+    //     ' de la historia geológica de C.Real.'
+    // })
+    // this.products.push({
+    //   name: 'Experiencias', image: '29368.jpg', shortDescription: 'Ciudad Real tiene muchos lugares por descubrir… ¡atreveteydejáte encandilar por su mágia tan  desconocida como sorprendente!',
+    //   description: 'Ciudad Real tiene muchos lugares por descubrir… ¡atreveteydejáte encandilar por su mágia tan  desconocida como sorprendente! Experiencias gastronómicas, paquetes de fin de semana, turismo de salud...'
+    // })
+    // this.products.push({
+    //   name: 'Viajes', image: '29369.jpg', shortDescription: 'Viajes y escapadas en grupo',
+    //   description: 'Viajes y escapadas en grupo. Si eres de la provincia y quieres conocer sitios nuevos de la Península Ibérica, pero no tienes tiempo para organizarlo, ¡no te preocupes! Realizamos circuitos para aventureros con diferentes inquietudes y con todo organizado. ;)'
+    // })
   }
 
   ngOnInit(): void {
-
+    this.genericService.httpPost(endpoint.CATEGORY_LIST, {}).subscribe(
+      list => this.categoryListSubject.next(list)
+    )
   }
 
 }
