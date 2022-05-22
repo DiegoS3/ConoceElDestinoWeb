5254036736b2cc8bd377b70aea7ae674c3a82764
Diego Sánchez <diegozj92@gmail.com>
2022-05-22 13:47:49 +0200

    footer form api

--- ConoceElDestino/src/app/shared/components/footer/footer.component.ts
+++ ConoceElDestino/src/app/shared/components/footer/footer.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
+import { BehaviorSubject, map, Observable } from 'rxjs';
+import { CategoryService } from 'src/app/services/category.service';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-footer',
@@ -12,66 +15,49 @@ export class FooterComponent implements OnInit {
 
   currentYear = new Date().getFullYear();
   private label_translate = '';
+  categoryList$: Observable<any[]>
 
-  footer = [
-    {
-      title: 'footer.titles.services',
-      navigate: '/details',
-      sections:
-        [
-          {
-            label: 'footer.guided_visits',
-            navigate: ''
-          },
-          {
-            label: 'footer.georoutes',
-            navigate: ''
-          },
-          {
-            label: 'footer.experiences',
-            navigate: ''
-          },
-          {
-            label: 'footer.travels',
-            navigate: ''
-          },
-        ]
-    },
-    {
-      title: 'footer.titles.about_us',
-      sections:
-        [
-          {
-            label: 'footer.about',
-            navigate: 'about'
-          },
-          {
-            label: 'footer.contact_us',
-            navigate: 'contact'
-          },
-          {
-            label: 'footer.work_us',
-            navigate: 'collaborators'
-          },
-          {
-            label: 'footer.privacy_policy',
-            navigate: ''
-          },
-          {
-            label: 'footer.terms_conditions',
-            navigate: ''
-          },
-        ]
+  private footerSubject = new BehaviorSubject<any[]>([]);
+  footer$: Observable<any[]>
 
-    },
-  ];
+  sectionAbout = {
+    title: 'footer.titles.about_us',
+    sections:
+      [
+        {
+          label: 'footer.about',
+          navigate: 'about'
+        },
+        {
+          label: 'footer.contact_us',
+          navigate: 'contact'
+        },
+        {
+          label: 'footer.work_us',
+          navigate: 'collaborators'
+        },
+        {
+          label: 'footer.privacy_policy',
+          navigate: ''
+        },
+        {
+          label: 'footer.terms_conditions',
+          navigate: ''
+        },
+      ]
+  }
 
   constructor(
     private translate: TranslateService,
-    private router: Router
-  ) { }
+    private router: Router,
+    private categoryService: CategoryService
+  ) {
+    this.categoryList$ = this.categoryService.categoryList$;
+    this.footer$ = this.footerSubject.asObservable();
+  }
 
   ngOnInit(): void {
+    this.generateFooter();
   }
 
   navigate(label: string, navigate?: string, navigate2?: string): void {
@@ -79,5 +65,27 @@ export class FooterComponent implements OnInit {
     sessionStorage.setItem('section', this.label_translate);
     const url = navigate ? this.label_translate + navigate : navigate2!;
     this.router.navigate([url]);
+  }
+
+  private generateFooter(): void {
+    this.categoryList$
+      .subscribe(categories => {
+
+        categories.map(category => {
+          this.footerSubject.next([
+            {
+              title: 'footer.titles.services',
+              navigate: '/details',
+              sections:
+                [
+                  {
+                    label: category.name,
+                    navigate: ''
+                  }]
+            },
+            this.sectionAbout,
+          ]);
+        });
+      });
   }
 }
